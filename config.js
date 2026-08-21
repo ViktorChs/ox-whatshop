71d2013b9c79f90deb11ca235dd6feeecc3f3c19
ViktorChs <victoratlas123@gmail.com>
2026-08-21 02:42:54 -0400

    Migrar OX WhatShop a Supabase + rutas relativas para GitHub Pages

---
+++ config.js
@@ -0,0 +1,4 @@
+/* Configuracion de Supabase - OX WhatShop */
+const SUPABASE_URL = 'https://qfxcnvnjbabikdikftsr.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_BRvDhGZ0uVwqxP3QL0vcYQ_NMuEAnYs';
+const STORAGE_BUCKET = 'images';
